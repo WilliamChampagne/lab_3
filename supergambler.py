2063e084ec02bcd39515a780ec1847df5c4eb38f
William <201963694@cegepsherbrooke.qc.ca>
2025-02-07 14:33:59 -0500

    Cacher debug prints
    cacher les debug prints

--- supergambler.py
+++ supergambler.py
@@ -14,21 +14,21 @@ def supergambler():
     rn1 = random.randint(1, 100)
     rn2 = random.randint(1, 100)
 
-    ## Debug print to see the value of rn1 and rn2
-    print("rn1: " + str(rn1), "rn2: " + str(rn2))
+    ## Debug print to see the value of rn1 and rn2 (remove double comment markers to print debug)
+    ## print("rn1: " + str(rn1), "rn2: " + str(rn2))
 
     ## Gameplay loop
     if n1 >= rn1 and n2 >= rn2:
-        print("Kaching! You won! C1")
+        print("Kaching! You won!")
         balance += bet*2
         supergambler()
     elif ((n1+n2)%rn2) > rn1:
         print((n1+n2)%rn2)
-        print("Kaching! You won! C2")
+        print("Kaching! You won!")
         balance += bet*2
         supergambler()
     elif n1 == rn2 or n2 == rn1:
-        print("Kaching! You won! C3")
+        print("Kaching! You won!")
         balance += bet*2
         supergambler()
     else:
