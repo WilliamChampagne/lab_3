ed28d5008a1cf2fe205e8d228a48eb185173d714
William <201963694@cegepsherbrooke.qc.ca>
2025-02-07 14:31:18 -0500

    Bug fix, simplification et explications
    Explications rajouter pour les lignes de codes ou sections de codes importantes
    
    Simplification et elimination de code redondant
    
    Fix des bugs par rapport au gameplay

--- supergambler.py
+++ supergambler.py
@@ -1,30 +1,35 @@
 import random
 balance = 1000
 
-
 def supergambler():
     global balance
     print("Your balance: " + str(balance))
+
+    ## Choosing your bet and your numbers
     bet = int(input("How much are you betting?: "))
     n1 = int(input("Choose your first number: "))
     n2 = int(input("Choose your second number: "))
+
+    ## Generating random numbers
     rn1 = random.randint(1, 100)
-    print(rn1)
-    print("rn1: " + str(rn1))
     rn2 = random.randint(1, 100)
-    print("rn2: " + str(rn2))
-    if n1 and n2 <= rn1:
+
+    ## Debug print to see the value of rn1 and rn2
+    print("rn1: " + str(rn1), "rn2: " + str(rn2))
+
+    ## Gameplay loop
+    if n1 >= rn1 and n2 >= rn2:
         print("Kaching! You won! C1")
-        balance = bet*2
+        balance += bet*2
         supergambler()
     elif ((n1+n2)%rn2) > rn1:
         print((n1+n2)%rn2)
         print("Kaching! You won! C2")
-        balance = bet*2
+        balance += bet*2
         supergambler()
-    elif n1 or n2 == rn1:
+    elif n1 == rn2 or n2 == rn1:
         print("Kaching! You won! C3")
-        balance = bet*2
+        balance += bet*2
         supergambler()
     else:
         print("You lost. Pay up.")
